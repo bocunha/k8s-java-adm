21dbb5380f8766e92b8c085f9a96712ca30fc150
EC2 Default User <ec2-user@ip-192-168-50-167.sa-east-1.compute.internal>
2021-11-24 18:22:36 +0000

    docker

--- docker-compose.sh
+++ docker-compose.sh
@@ -17,7 +17,7 @@ mvn package
 echo "
 FROM openjdk:8-jdk-alpine
 ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
+COPY \${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
 " > Dockerfile
 
